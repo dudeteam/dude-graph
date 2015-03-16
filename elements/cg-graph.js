94c56312750729fcd58ea9128aace6f3b272e82c
Jeremie Taboada <taboada.jeremie@gmail.com>
2015-03-16 16:13:45 +0000

    Add action.

--- elements/cg-graph.js
+++ elements/cg-graph.js
@@ -38,65 +38,79 @@ Polymer({
         this.element.convertSheetsToStyles(this.shadowRoot);
     },
 
+    handleErrors: function () {
+        this.loader.on("error", function (error) { this.showError(error.message); }.bind(this));
+        this.saver.on("error", function (error) { this.showError(error); }.bind(this));
+        this.graph.on("error", function (error) { this.showError(error); }.bind(this));
+    },
+
+    handleKeyboard: function () {
+        jwerty.key('alt+A', function () {
+            this.modelList = this.graph.findModels();
+            this.$.actionDialog.open();
+        }.bind(this));
+        jwerty.key('alt+G', function () {
+            this.$.groupDialog.open();
+        }.bind(this));
+        jwerty.key('alt+X', function () {
+            this.renderer.removeSelectedNodes();
+        }.bind(this));
+    },
+
     showError: function (message) {
         this.$.error.text = message;
         this.$.error.show();
     },
 
+    filterModels: function (modelPattern) {
+        console.log(modelPattern);
+        return false;
+    },
+
     createGroup: function () {
         var _id = graph.getNextGroupId();
-        renderer.createGroupFromSelection(_id, this.groupTitle);
+        renderer.createGroupFromSelection(_id, this.groupTitle
+        );
+    },
+
+    createAction: function () {
+        var _id = this.graph.getNextBlockId();
+        var modelName = "mix";
+        var model = this.graph.getModel(modelName);
+        this.graph.addNode(new cg.Block(_id, model, this.renderer._mousePosition.clone()), this.graph);
     },
 
     /**
      * Create the graph when its config and data are loaded.
      */
     createGraph: function () {
-        var paper = Snap(this.$.graph);
-        var graph = new cg.Graph();
-        var loader = new cg.JSONLoader();
-        var saver = new cg.JSONSaver();
-        var renderer = new cg.Renderer(paper);
-        loader.on("error", function (error) { this.showError(error.message); }.bind(this));
-        saver.on("error", function (error) { this.showError(error); }.bind(this));
-        graph.on("error", function (error) { this.showError(error); }.bind(this));
-        renderer.on("picker.edit", function (picker) {
+        this.renderer.on("picker.edit", function (picker) {
             var value = prompt("New value: ");
             if (picker.model.valueType === "vec2" || picker.model.valueType === "vec3") {
                 value = value.split(" ");
             }
             picker.value = value;
         });
-        renderer.initialize(graph, this.config);
-        loader.load(graph, this.data);
-        renderer.render(graph);
-        jwerty.key('A', function () {
-            var _id = graph.getNextBlockId();
-            var modelName = prompt("model");
-            var model = graph.getModel(modelName);
-            if (model === undefined) {
-                alert("Undefined model " + modelName + ".");
-            } else {
-                var action = new cg.Block(_id, model, renderer._mousePosition.clone());
-                graph.addNode(action, graph);
-            }
-        });
-        jwerty.key('G', function () {
-            this.$.group.open();
-        }.bind(this));
-        jwerty.key('X', function () {
-            renderer.removeSelectedNodes();
-        });
-        window.graph = graph;
-        window.renderer = renderer;
-        window.loader = loader;
-        window.saver = saver;
+        this.renderer.initialize(this.graph, this.config);
+        this.loader.load(this.graph, this.data);
+        this.renderer.render(this.graph);
     },
 
     /**
      * Load config and data then call createGraph().
      */
     ready: function () {
+        this.paper = Snap(this.$.graph);
+        this.graph = new cg.Graph();
+        this.loader = new cg.JSONLoader();
+        this.saver = new cg.JSONSaver();
+        this.renderer = new cg.Renderer(this.paper);
+        window.graph = this.graph;
+        window.renderer = this.renderer;
+        window.loader = this.loader;
+        window.saver = this.saver;
+        this.handleErrors();
+        this.handleKeyboard();
         this.addTheme(this.theme);
         this.$.config.addEventListener("core-complete", function () {
             this.config = this.$.config.response;
@@ -110,7 +124,7 @@ Polymer({
                 this.createGraph();
             }
         }.bind(this));
-        this.$.group.addEventListener("core-overlay-close-completed", function () {
+        this.$.groupDialog.addEventListener("core-overlay-close-completed", function () {
             this.groupTitle = "";
         }.bind(this));
     }
