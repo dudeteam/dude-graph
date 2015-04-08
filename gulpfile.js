6a3968d9dd546779f3995f244a41edf231d87fcf
JonathanPicques <jonathan.picques@gmail.com>
2015-04-08 14:38:38 +0100

    Updated gulpfile to allow watch

--- gulpfile.js
+++ gulpfile.js
@@ -3,52 +3,59 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var jshint = require('gulp-jshint');
 
-/**
- * Uglify the Javascript code.
- * @type {boolean}
- */
-var doUglify = false;
-
 var paths = {
     "lib": "./lib/",
-    "dist": "./dist/"
+    "dist": "."
 };
 
 var sources = [
     "index.js",
     "graph/utils/graph-error.js",
-    "graph/nodes/node.js",
+    "graph/nodes/entity.js",
     "graph/utils/container.js",
+    "graph/nodes/group.js",
+    "graph/nodes/point.js",
     "graph/models/model.js",
     "graph/models/getter.js",
     "graph/models/picker.js",
     "graph/models/action.js",
-    "graph/nodes/group.js",
-    "graph/nodes/point.js",
     "graph/nodes/block.js",
     "graph/nodes/connection.js",
     "graph/nodes/graph.js",
-    "renderer/renderer-error.js",
-    "renderer/index.js",
-    "renderer/cursor-point.js",
-    "renderer/utils.js",
-    "renderer/zoom.js",
-    "renderer/grid.js",
-    "renderer/render-graph.js",
-    "renderer/render-container.js",
-    "renderer/render-group.js",
-    "renderer/render-block.js",
-    "renderer/render-point.js",
-    "renderer/render-connection.js",
+    "renderer/renderer.js",
+    "renderer/utils/renderer-d3.js",
+    "renderer/utils/renderer-error.js",
+    "renderer/utils/renderer-position.js",
+    "renderer/utils/renderer-point.js",
+    "renderer/utils/renderer-collision.js",
+    "renderer/render/render.js",
+    "renderer/render/render-selection.js",
+    "renderer/render/render-zoom.js",
+    "renderer/render/render-drag.js",
+    "renderer/render/render-block.js",
+    "renderer/render/render-picker.js",
+    "renderer/render/render-group.js",
+    "renderer/render/render-connection.js",
+    "renderer/render/render-point.js",
     "serialization/json-saver.js",
     "serialization/json-loader.js"
 ];
 
-gulp.task('default', function() {
-    return gulp.src(sources.map(function(e) { return paths.lib + e; }))
+gulp.task('watch', function () {
+    gulp.watch(sources.map(function (e) {
+        return paths.lib + e;
+    }), ['build']);
+});
+
+gulp.task('build', function () {
+    return gulp.src(sources.map(function (e) {
+        return paths.lib + e;
+    }))
         .pipe(jshint())
         .pipe(jshint.reporter('default'))
-        .pipe(concat("codegraph.min.js"))
-        .pipe(uglify({mangle: false}))
+        .pipe(concat("codegraph.js"))
+        //.pipe(uglify({mangle: false}))
         .pipe(gulp.dest(paths.dist));
 });
+
+gulp.task('library', ['build', 'watch']);
