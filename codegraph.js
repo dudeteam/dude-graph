bd1a46e3d25a671304265575bd63345543bc2eaf
JonathanPicques <jonathan.picques@gmail.com>
2015-05-23 16:15:01 +0100

    Added configurable number of connections for a point, closes #62

--- codegraph.js
+++ codegraph.js
@@ -226,15 +226,16 @@ cg.Value = (function () {
 cg.Point = (function () {
 
     /**
-     * Represent a point into the graph to connect an action to another.
+     * Represent a point into the graph to connect a block to another.
      * @param block {cg.Block} The block on which the point is attached.
      * @param index {Number} The vertical index of the point within the block.
      * @param type {Number} The type of this connection (e.g. Number, Color, etc...).
      * @param name {Number} The name of this connection.
+     * @param max {Number} Maximum number of connection for this point.
      * @param isInput {Boolean} Whether the point is an input or an output.
      * @constructor
      */
-    var Point = pandora.class_("Point", pandora.EventEmitter, function (block, index, type, name, isInput) {
+    var Point = pandora.class_("Point", pandora.EventEmitter, function (block, index, type, name, max, isInput) {
         pandora.EventEmitter.call(this);
 
         /**
@@ -248,7 +249,7 @@ cg.Point = (function () {
         });
 
         /**
-         * Point index within the action.
+         * Point index within the block.
          * @type {Number}
          * @private
          */
@@ -275,6 +276,19 @@ cg.Point = (function () {
         this._name = name;
         Object.defineProperty(this, "name", {
             get: function () { return this._name; }.bind(this)
+        });
+
+        /**
+         * Maximum number of connections for this point.
+         * -1 = Unlimited
+         * 0 = Point disabled
+         * 1+ = Number of connections
+         * @type {number}
+         * @private
+         */
+        this._max = max || 1;
+        Object.defineProperty(this, "max", {
+            get: function() { return this._max; }.bind(this)
         });
 
         /**
@@ -308,6 +322,15 @@ cg.Point = (function () {
             set: function (data) { this._data = data; }.bind(this)
         });
     });
+
+    /**
+     * Returns whether this point accept a the specified connection.
+     * @param {cg.Connection} connection
+     * @returns {Boolean}
+     */
+    Point.prototype.acceptConnection = function(connection) {
+        return this.max !== 0 && (this.max === -1 || this.connections.length < this.max);
+    };
 
     /**
      * Add a connection for this point, this is called by graph.addConnection(connection) and dispatched here if the connection has this point.
@@ -777,10 +800,10 @@ cg.Block = (function () {
      */
     Block.prototype.initialize = function() {
         pandora.forEach(this._model.inputs, function (input, index) {
-            this._inputs.push(new cg.Point(this, index, input.type, input.name, true));
+            this._inputs.push(new cg.Point(this, index, input.type, input.name, input.max, true));
         }.bind(this));
         pandora.forEach(this._model.outputs, function (output, index) {
-            this._outputs.push(new cg.Point(this, index, output.type, output.name, false));
+            this._outputs.push(new cg.Point(this, index, output.type, output.name, output.max, false));
         }.bind(this));
     };
 
@@ -1106,6 +1129,14 @@ cg.Graph = (function () {
         var graphForOutput = connection.outputPoint.block.graph;
         if (graphForInput !== this || graphForOutput !== this) {
             this.emit("error", new cg.GraphError("This connection does not belong to this graph"));
+            return null;
+        }
+        if (!connection.inputPoint.acceptConnection(connection)) {
+            this.emit("error", new cg.GraphError("The input point only accepts {0} connection(s)", connection.inputPoint.max));
+            return null;
+        }
+        if (!connection.outputPoint.acceptConnection(connection)) {
+            this.emit("error", new cg.GraphError("The output point only accepts {0} connection(s)", connection.outputPoint.max));
             return null;
         }
         if (connection.inputPoint.type !== connection.outputPoint.type) {
