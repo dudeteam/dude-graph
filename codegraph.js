45fa33f07b4e7d8482a04f44cec4c89a56f73e3d
Jeremie Taboada <taboada.jeremie@gmail.com>
2015-07-21 16:53:06 +0100

    Add simple block and group rendering.

--- codegraph.js
+++ codegraph.js
@@ -1633,29 +1633,46 @@ cg.Renderer = (function () {
      * Creates the svg graph from the given cgGraph. This method will also listen the graph's events in order to
      * updated the rendered svg graph.
      * @param el The canvas DOM Element on which the svg will be append
+     * @param data The serialized renderer elements
      * @param cgGraph The graph that will be rendered
      */
-    Renderer.prototype.create = function (el, cgGraph) {
+    Renderer.prototype.create = function (el, data, cgGraph) {
         var svg = d3.select(el);
-        var root = svg.append("svg:g").attr("id", "root");
-        this._createGroups(root, cgGraph);
-        this._createConnections(root, cgGraph);
-        this._createBlocks(root, cgGraph);
+        var root = svg.append("svg:g").attr("id", "cgRoot");
+        this._createGroups(root, data, cgGraph);
+        this._createBlocks(root, data, cgGraph);
     };
 
-    Renderer.prototype._createGroups = function (root, cgGraph) {
-        var groupLayer = root.append("svg:g").attr("id", "groupLayer");
-        return groupLayer;
+    Renderer.prototype._createGroups = function (root, data, cgGraph) {
+        root.append("svg:g").attr("id", "cgGroups")
+            .selectAll(".cg-group")
+            .data(data.cgGroups, function (cgGroup) {
+                return cgGroup.cgId;
+            })
+            .enter()
+            .append("svg:g")
+            .attr("class", "cg-group")
+            .append("svg:rect")
+            .attr("transform", function (cgGroup) { return "translate(" + cgGroup.cgPosition + ")"; })
+            .attr("rx", 5).attr("ry", 5)
+            .attr("width", function (cgGroup) { return cgGroup.cgSize[0]; })
+            .attr("height", function (cgGroup) { return cgGroup.cgSize[1]; });
     };
 
-    Renderer.prototype._createConnections = function (root, cgGraph) {
-        var connectionLayer = root.append("svg:g").attr("id", "connectionLayer");
-        return connectionLayer;
-    };
-
-    Renderer.prototype._createBlocks = function (root, cgGraph) {
-        var blockLayer = root.append("svg:g").attr("id", "blockLayer");
-        return blockLayer;
+    Renderer.prototype._createBlocks = function (root, data, cgGraph) {
+        root.append("svg:g").attr("id", "cgBlocks")
+            .selectAll(".cg-block")
+            .data(data.cgBlocks, function (cgBlock) {
+                return cgBlock.cgId;
+            })
+            .enter()
+            .append("svg:g")
+                .attr("class", "cg-block")
+                .append("svg:rect")
+                    .attr("transform", function (block) { return "translate(" + block.cgPosition + ")"; })
+                    .attr("rx", 5).attr("ry", 5)
+                    .attr("width", function () { return 100; })
+                    .attr("height", function () { return 100; });
     };
 
     return Renderer;
