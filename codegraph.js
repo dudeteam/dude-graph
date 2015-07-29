95f27d3c32867a3d75e7922be896e310c0230150
Jeremie Taboada <taboada.jeremie@gmail.com>
2015-07-29 12:09:17 +0100

    Handled getters. Used ReturnType for functions and instructions. Added entity related blocks in models.

--- codegraph.js
+++ codegraph.js
@@ -1648,29 +1648,94 @@ cg.Each = (function () {
 cg.Function = (function () {
 
     /**
-     *
+     * This block represents a simple function that takes some inputs and returns one or zero output.
      * @extends {cg.Block}
      * @param cgGraph {cg.Graph}
      * @param data {Object}
      * @constructor
      */
     var Function = pandora.class_("Function", cg.Block, function (cgGraph, data) {
-        cg.Block.call(this, cgGraph, data);
+        cg.Block.call(this, cgGraph, {
+            cgId: data.cgId,
+            cgName: data.cgName,
+            cgInputs: data.cgInputs,
+            cgOutputs: data.cgReturnType ? [{
+                cgType: "Point",
+                cgName: "value",
+                cgValueType: data.cgReturnType
+            }] : null
+        });
     });
 
     return Function;
 
 })();
+cg.Getter = (function () {
+
+    /**
+     * This block represents a simple Getter that takes some inputs and returns one or zero output.
+     * @extends {cg.Block}
+     * @param cgGraph {cg.Graph}
+     * @param data {Object}
+     * @constructor
+     */
+    var Getter = pandora.class_("Getter", cg.Block, function (cgGraph, data) {
+        if (data.cgClassType === undefined) {
+            throw new cg.GraphError("Getter `{0}` should specify a class type");
+        }
+        if (data.cgValueType === undefined) {
+            throw new cg.GraphError("Getter `{0}` should specify a value type");
+        }
+        cg.Block.call(this, cgGraph, {
+            cgId: data.cgId,
+            cgName: data.cgName,
+            cgModel: data.cgModel,
+            cgInputs: [{
+                cgType: "Point",
+                cgName: "this",
+                cgValueType: data.cgClassType
+            }],
+            cgOutputs: [{
+                cgType: "Point",
+                cgName: "value",
+                cgValueType: data.cgValueType
+            }]
+        });
+    });
+
+    return Getter;
+
+})();
 cg.Instruction = (function () {
 
     /**
-     *
+     * This is like function however, it takes a stream in input and output. In code it would represent function
+     * separated by semicolons.
      * @extends {cg.Block}
      * @param cgGraph {cg.Graph}
      * @param data {Object}
      * @constructor
      */
     var Instruction = pandora.class_("Instruction", cg.Block, function (cgGraph, data) {
+        data.cgInputs.unshift({
+            "cgType": "Point",
+            "cgName": "in",
+            "cgValueType": "Stream"
+        });
+        data.cgOutputs = [
+            {
+                "cgType": "Point",
+                "cgName": "out",
+                "cgValueType": "Stream"
+            }
+        ];
+        if (data.cgReturnType) {
+            data.cgOutputs.push({
+                "cgType": "Point",
+                "cgName": "value",
+                "cgValueType": data.cgReturnType
+            });
+        }
         cg.Block.call(this, cgGraph, data);
     });
 
@@ -1843,7 +1908,12 @@ cg.JSONLoader = (function () {
         this._blockTypes = {};
         this.addBlockType(cg.Block);
         this.addBlockType(cg.Function);
+        this.addBlockType(cg.Instruction);
         this.addBlockType(cg.Variable);
+        this.addBlockType(cg.Each);
+        this.addBlockType(cg.Range);
+        this.addBlockType(cg.Condition);
+        this.addBlockType(cg.Getter);
         this.addPointType(cg.Stream);
         this.addPointType(cg.Point);
     });
