741be7829dd58d8625bdd2c46e7d0529f88da432
JonathanPicques <jonathan.picques@gmail.com>
2015-08-05 17:58:33 +0100

    Added update* methods for d3Connections

--- codegraph.js
+++ codegraph.js
@@ -2908,39 +2908,69 @@ cg.Renderer.prototype._removeD3Blocks = function () {
  * @private
  */
 cg.Renderer.prototype._createD3Connections = function () {
-    this._connectionsSvg.selectAll(".cg-connection")
+    var createdD3Connections = this._connectionsSvg.selectAll(".cg-connection")
         .data(this._cgGraph.cgConnections)
         .enter()
         .append("svg:path")
-            .attr("class", "cg-connection")
-            .classed("cg-stream", function (cgConnection) {
-                return pandora.typename(cgConnection.cgInputPoint) === "Stream";
-            })
-            .attr("d", function (cgConnection) {
-                var p1 = this._getOutputPosition(cgConnection.cgOutputPoint);
-                var p2 = this._getInputPosition(cgConnection.cgInputPoint);
-                var step = Math.max(0.5 * (p1[0] - p2[1]) + 100, 50);
-                return pandora.formatString("M{x},{y}C{x1},{y1} {x2},{y2} {x3},{y3}", {
-                    x: p1[0], y: p1[1],
-                    x1: p1[0] + step, y1: p1[1],
-                    x2: p2[0] - step, y2: p2[1],
-                    x3: p2[0], y3: p2[1]
-                });
-            }.bind(this))
+        .attr("class", "cg-connection")
+        .classed("cg-stream", function (cgConnection) {
+            return pandora.typename(cgConnection.cgInputPoint) === "Stream";
+        });
+    this._updateSelectedD3Connections(createdD3Connections);
 };
 
-cg.Renderer.prototype._getOutputPosition = function (point) {
-    var rendererBlock = this._getRendererBlockById(point.cgBlock.cgId);
-    var index = point.cgBlock.cgOutputs.indexOf(point.cgBlock.outputByName(point.cgName));
+/**
+ * Updates all d3Connections
+ * @private
+ */
+cg.Renderer.prototype._updatedD3Connections = function () {
+    this._updateSelectedD3Connections(this._connectionsSvg.selectAll(".cg-connection"));
+};
+
+/**
+ * Updates selected d3Connections
+ * @param updatedD3Connections
+ * @private
+ */
+cg.Renderer.prototype._updateSelectedD3Connections = function (updatedD3Connections) {
+    updatedD3Connections
+        .attr("d", function (cgConnection) {
+            var p1 = this._getOutputPosition(cgConnection.cgOutputPoint);
+            var p2 = this._getInputPosition(cgConnection.cgInputPoint);
+            var step = Math.max(0.5 * (p1[0] - p2[1]) + 100, 50);
+            return pandora.formatString("M{x},{y}C{x1},{y1} {x2},{y2} {x3},{y3}", {
+                x: p1[0], y: p1[1],
+                x1: p1[0] + step, y1: p1[1],
+                x2: p2[0] - step, y2: p2[1],
+                x3: p2[0], y3: p2[1]
+            });
+        }.bind(this))
+};
+
+/**
+ *
+ * @param cgPoint {cg.Point}
+ * @returns {[Number, Number]}
+ * @private
+ */
+cg.Renderer.prototype._getOutputPosition = function (cgPoint) {
+    var rendererBlock = this._getRendererBlockById(cgPoint.cgBlock.cgId);
+    var index = cgPoint.cgBlock.cgOutputs.indexOf(cgPoint.cgBlock.outputByName(cgPoint.cgName));
     return [
         rendererBlock.position[0] + rendererBlock.size[0] - this._config.block.padding,
         rendererBlock.position[1] + this._config.block.header + this._config.point.height * index
     ];
 };
 
-cg.Renderer.prototype._getInputPosition = function (point) {
-    var rendererBlock = this._getRendererBlockById(point.cgBlock.cgId);
-    var index = point.cgBlock.cgInputs.indexOf(point.cgBlock.inputByName(point.cgName));
+/**
+ *
+ * @param cgPoint {cg.Point}
+ * @returns {[Number, Number]}
+ * @private
+ */
+cg.Renderer.prototype._getInputPosition = function (cgPoint) {
+    var rendererBlock = this._getRendererBlockById(cgPoint.cgBlock.cgId);
+    var index = cgPoint.cgBlock.cgInputs.indexOf(cgPoint.cgBlock.inputByName(cgPoint.cgName));
     return [
         rendererBlock.position[0] + this._config.block.padding,
         rendererBlock.position[1] + this._config.block.header + this._config.point.height * index
