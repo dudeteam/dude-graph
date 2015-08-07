8c984da245383dff8d75ea1a607ca2963a9be318
JonathanPicques <jonathan.picques@gmail.com>
2015-08-07 09:47:29 +0100

    Added double click to remove rendererNode from parent

--- codegraph.js
+++ codegraph.js
@@ -2608,6 +2608,74 @@ cg.Renderer.prototype._getNearestCgPoint = function (position) {
     return null;
 };
 /**
+ * Internal function of d3
+ * @param dispatch
+ * @returns {event}
+ */
+function d3_dispatch_event(dispatch) {
+    var listeners = [], listenerByName = d3.map();
+
+    function event() {
+        var z = listeners, i = -1, n = z.length, l;
+        while (++i < n) if (l = z[i].on) l.apply(this, arguments);
+        return dispatch;
+    }
+
+    event.on = function (name, listener) {
+        var l = listenerByName.get(name), i;
+        if (arguments.length < 2) return l && l.on;
+        if (l) {
+            l.on = null;
+            listeners = listeners.slice(0, i = listeners.indexOf(l)).concat(listeners.slice(i + 1));
+            listenerByName.remove(name);
+        }
+        if (listener) listeners.push(listenerByName.set(name, {on: listener}));
+        return dispatch;
+    };
+    return event;
+}
+
+/**
+ * Internal function of d3
+ * @param target
+ */
+function d3_eventDispatch(target) {
+    var dispatch = d3.dispatch(), i = 0, n = arguments.length;
+    while (++i < n) dispatch[arguments[i]] = d3_dispatch_event(dispatch);
+    dispatch.of = function (thiz, argumentz) {
+        return function (e1) {
+            try {
+                var e0 = e1.sourceEvent = d3.event;
+                e1.target = target;
+                d3.event = e1;
+                dispatch[e1.type].apply(thiz, argumentz);
+            } finally {
+                d3.event = e0;
+            }
+        };
+    };
+    return dispatch;
+}
+
+/**
+ * Double click behavior
+ */
+d3.behavior.doubleClick = function () {
+    var event = d3_eventDispatch(strangeBehavior, "dblclick");
+    function strangeBehavior(selection) {
+        selection.each(function (i) {
+            var dispatch = event.of(this, arguments);
+            d3.select(this).on("dblclick", clicked);
+            function clicked() {
+                dispatch({
+                    "type": "dblclick"
+                });
+            }
+        });
+    }
+    return d3.rebind(strangeBehavior, event, "on");
+};
+/**
  * Creates the drag and drop behavior on a d3Node
  * @returns {d3.behavior.drag}
  * @private
@@ -2616,6 +2684,7 @@ cg.Renderer.prototype._createDragBehavior = function () {
     var renderer = this;
     return d3.behavior.drag()
         .on("dragstart", function () {
+            d3.event.sourceEvent.preventDefault();
             d3.event.sourceEvent.stopPropagation();
             renderer._addToSelection(d3.select(this), !d3.event.sourceEvent.shiftKey);
             renderer._d3MoveToFront(renderer.d3GroupedSelection);
@@ -2647,6 +2716,29 @@ cg.Renderer.prototype._createDragBehavior = function () {
 };
 
 /**
+ * Creates the remove parent behavior on double click
+ * @returns {d3.behavior.doubleClick}
+ * @private
+ */
+cg.Renderer.prototype._createRemoveParentBehavior = function () {
+    var renderer = this;
+    return d3.behavior.doubleClick()
+        .on("dblclick", function () {
+            var d3Node = d3.select(this);
+            var rendererNode = d3Node.datum();
+            var rendererGroupParent = rendererNode.parent;
+            if (rendererGroupParent) {
+                d3.event.sourceEvent.preventDefault();
+                d3.event.sourceEvent.stopPropagation();
+                renderer._removeRendererNodeParent(rendererNode);
+                // renderer._updateSelectedD3Nodes(renderer._getD3NodesFromRendererNodes([rendererNode, rendererGroupParent]));
+                // TODO: Optimize
+                renderer._updateSelectedD3Nodes(renderer.d3Nodes);
+            }
+        });
+};
+
+/**
  * Creates the placement behavior when a new rendererBlock is added
  * @param d3Block {d3.selection}
  * @private
@@ -2660,10 +2752,13 @@ cg.Renderer.prototype._createPlacementBehavior = function (d3Block) {
     };
     disablePlacement();
     this._svg.on("mousemove" + namespace, function () {
+        d3.event.sourceEvent.preventDefault();
         d3Block.datum().position = renderer._getRelativePosition(d3.mouse(this));
         renderer._updateSelectedD3Nodes(d3Block);
     });
     this._svg.on("mousedown" + namespace, function () {
+        d3.event.sourceEvent.preventDefault();
+        d3.event.sourceEvent.stopPropagation();
         d3Block.datum().position = renderer._getRelativePosition(d3.mouse(this));
         renderer._updateSelectedD3Nodes(d3Block);
         disablePlacement();
@@ -2912,7 +3007,8 @@ cg.Renderer.prototype._createD3Blocks = function () {
             return this._getRendererNodeUniqueID(rendererBlock);
         }.bind(this))
         .attr("class", "cg-block")
-        .call(this._createDragBehavior());
+        .call(this._createDragBehavior())
+        .call(this._createRemoveParentBehavior());
     createdD3Blocks
         .append("svg:rect");
     createdD3Blocks
@@ -3030,7 +3126,8 @@ cg.Renderer.prototype._createD3Groups = function () {
             return this._getRendererNodeUniqueID(rendererGroup);
         }.bind(this))
         .attr("class", "cg-group")
-        .call(this._createDragBehavior());
+        .call(this._createDragBehavior())
+        .call(this._createRemoveParentBehavior());
     createdD3Groups
         .append("svg:rect");
     createdD3Groups
