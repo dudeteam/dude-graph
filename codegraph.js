037811487002289a5aa3e8d25b3996267b4bacb7
Jeremie Taboada <taboada.jeremie@gmail.com>
2015-08-14 15:29:59 +0100

    Added RendererSaver.

--- codegraph.js
+++ codegraph.js
@@ -2200,13 +2200,35 @@ cg.RendererSaver = (function() {
         this.config = config;
     });
 
-    RendererSaver.prototype.save = function (value, tabs) {
-        return pandora.polymorphicMethod(this, "save", value, tabs);
-    };
-
-    RendererSaver.prototype._saveRenderer = function (renderer) {
-        var result = {};
-        //console.log(renderer);
+    RendererSaver.prototype.save = function (renderer) {
+        var result = {
+            "blocks": [],
+            "groups": [],
+            "connections": []
+        };
+        pandora.forEach(renderer._rendererBlocks, function (rendererBlock) {
+            result.blocks.push({
+                "id": rendererBlock.id,
+                "cgBlock": rendererBlock.cgBlock.cgId,
+                "position": rendererBlock.position,
+                "parent": rendererBlock.parent ? rendererBlock.parent.id : null
+            });
+        });
+        pandora.forEach(renderer._rendererGroups, function (rendererGroup) {
+            result.groups.push({
+                "id": rendererGroup.id,
+                "description": rendererGroup.description,
+                "parent": rendererGroup.parent ? rendererGroup.parent.id : null
+            });
+        });
+        pandora.forEach(renderer._rendererConnections, function (rendererConnection) {
+            result.connections.push({
+                "outputName": rendererConnection.outputPoint.cgPoint.cgName,
+                "outputBlockId": rendererConnection.outputPoint.rendererBlock.id,
+                "inputName": rendererConnection.inputPoint.cgPoint.cgName,
+                "inputBlockId": rendererConnection.inputPoint.rendererBlock.id
+            });
+        });
         return result;
     };
 
