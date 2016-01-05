e970da92ccf104ad8bf8e7af472b21058d8c8436
JonathanPicques <jonathan.picques@gmail.com>
2016-01-05 15:07:24 +0100

    Added back edit renderBlock points Started to work on toolbar

---
+++ demo/electron/main.js
@@ -0,0 +1,20 @@
+"use strict";
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+var mainWindow;
+
+app.on("window-all-closed", function () {
+    if (process.platform != "darwin") {
+        app.quit();
+    }
+});
+
+app.on("ready", function () {
+    mainWindow = new BrowserWindow({width: 800, height: 600});
+    mainWindow.loadURL("file://" + __dirname + "/index.html");
+    mainWindow.on("closed", function () {
+        mainWindow = null;
+    });
+});
