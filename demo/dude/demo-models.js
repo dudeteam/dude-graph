5be5bd7140c8aa25588b562b6beec6ae39df6094
Jeremie Taboada <taboada.jeremie@gmail.com>
2016-02-12 02:24:47 +0100

    Improved models.

---
+++ demo/dude/demo-models.js
@@ -0,0 +1,411 @@
+var DEMO_DUDE_MODEL_DATA = [
+    {
+        "item": {
+            "name": "New variable...",
+            "icon": "fa fa-plus-circle"
+        }
+    },
+    {
+        "item": {
+            "name": "New macro...",
+            "icon": "fa fa-plus-square"
+        }
+    },
+    {
+        "group": {
+            "name": "Variables",
+            "items": []
+        }
+    },
+    {
+        "group": {
+            "name": "Delegates",
+            "items": [
+                {
+                    "item": {
+                        "name": "on_start",
+                        "icon": "fa fa-play",
+                        "data": {
+                            "cgType": "Delegate",
+                            "cgName": "on_start",
+                            "cgInputs": [
+                            ],
+                            "cgOutputs": [
+                                {
+                                    "cgType": "Stream",
+                                    "cgName": "out"
+                                }
+                            ]
+                        }
+                    }
+                },
+                {
+                    "item": {
+                        "name": "on_update",
+                        "icon": "fa fa-play",
+                        "data": {
+                            "cgType": "Delegate",
+                            "cgName": "on_update",
+                            "cgInputs": [
+                            ],
+                            "cgOutputs": [
+                                {
+                                    "cgType": "Stream",
+                                    "cgName": "out"
+                                }
+                            ]
+                        }
+                    }
+                },
+                {
+                    "item": {
+                        "name": "on_stop",
+                        "icon": "fa fa-play",
+                        "data": {
+                            "cgType": "Delegate",
+                            "cgName": "on_update",
+                            "cgInputs": [
+                            ],
+                            "cgOutputs": [
+                                {
+                                    "cgType": "Stream",
+                                    "cgName": "out"
+                                }
+                            ]
+                        }
+                    }
+                },
+                {
+                    "item": {
+                        "name": "on_key_down",
+                        "icon": "fa fa-play",
+                        "data": {
+                            "cgType": "Delegate",
+                            "cgName": "on_key_down",
+                            "cgInputs": [
+                            ],
+                            "cgOutputs": [
+                                {
+                                    "cgType": "Stream",
+                                    "cgName": "out"
+                                },
+                                {
+                                    "cgType": "Point",
+                                    "cgValueType": "Choice",
+                                    "cgName": "key"
+                                }
+                            ]
+                        }
+                    }
+                },
+                {
+                    "item": {
+                        "name": "on_key_up",
+                        "icon": "fa fa-play",
+                        "data": {
+                            "cgType": "Delegate",
+                            "cgName": "on_key_down",
+                            "cgInputs": [
+                            ],
+                            "cgOutputs": [
+                                {
+                                    "cgType": "Stream",
+                                    "cgName": "out"
+                                },
+                                {
+                                    "cgType": "Point",
+                                    "cgValueType": "Choice",
+                                    "cgName": "key"
+                                }
+                            ]
+                        }
+                    }
+                },
+                {
+                    "item": {
+                        "name": "on_key",
+                        "icon": "fa fa-play",
+                        "data": {
+                            "cgType": "Delegate",
+                            "cgName": "on_key_down",
+                            "cgInputs": [
+                            ],
+                            "cgOutputs": [
+                                {
+                                    "cgType": "Stream",
+                                    "cgName": "out"
+                                },
+                                {
+                                    "cgType": "Point",
+                                    "cgValueType": "Choice",
+                                    "cgName": "key"
+                                }
+                            ]
+                        }
+                    }
+                }
+            ]
+        }
+    },
+    {
+        "group": {
+            "name": "Functions",
+            "items": [
+                {
+                    "item": {
+                        "name": "assign",
+                        "icon": "fa fa-square",
+                        "data": {
+                            "cgType": "Instruction",
+                            "cgName": "assign",
+                            "cgTemplates": {
+                                "ValueType": ["Boolean", "Number", "String"]
+                            },
+                            "cgInputs": [
+                                {
+                                    "cgType": "Stream",
+                                    "cgName": "in"
+                                },
+                                {
+                                    "cgType": "Point",
+                                    "cgName": "this",
+                                    "cgTemplate": "ValueType",
+                                    "cgValueType": "Number"
+                                },
+                                {
+                                    "cgType": "Point",
+                                    "cgName": "other",
+                                    "cgTemplate": "ValueType",
+                                    "cgValueType": "Number"
+                                }
+                            ],
+                            "cgOutputs": [
+                                {
+                                    "cgType": "Stream",
+                                    "cgName": "out"
+                                }
+                            ]
+                        }
+                    }
+                },
+                {
+                    "item": {
+                        "name": "add_entity",
+                        "icon": "fa fa-square",
+                        "data": {
+                            "cgType": "Instruction",
+                            "cgName": "add_entity",
+                            "cgInputs": [
+                                {
+                                    "cgType": "Stream",
+                                    "cgName": "in"
+                                },
+                                {
+                                    "cgType": "Point",
+                                    "cgName": "entity",
+                                    "cgValueType": "Entity"
+                                }
+                            ],
+                            "cgOutputs": [
+                                {
+                                    "cgType": "Stream",
+                                    "cgName": "out"
+                                }
+                            ]
+                        }
+                    }
+                },
+                {
+                    "item": {
+                        "name": "remove_entity",
+                        "icon": "fa fa-square",
+                        "data": {
+                            "cgType": "Instruction",
+                            "cgName": "remove_entity",
+                            "cgInputs": [
+                                {
+                                    "cgType": "Stream",
+                                    "cgName": "in"
+                                },
+                                {
+                                    "cgType": "Point",
+                                    "cgName": "entity",
+                                    "cgValueType": "Entity"
+                                }
+                            ],
+                            "cgOutputs": [
+                                {
+                                    "cgType": "Stream",
+                                    "cgName": "out"
+                                }
+                            ]
+                        }
+                    }
+                },
+                {
+                    "item": {
+                        "name": "random",
+                        "icon": "fa fa-square",
+                        "data": {
+                            "cgType": "Function",
+                            "cgName": "random_range",
+                            "cgInputs": [
+                                {
+                                    "cgType": "Point",
+                                    "cgName": "from",
+                                    "cgValueType": "Number"
+                                },
+                                {
+                                    "cgType": "Point",
+                                    "cgName": "to",
+                                    "cgValueType": "Number"
+                                }
+                            ],
+                            "cgOutputs": [
+                                {
+                                    "cgType": "Point",
+                                    "cgName": "value",
+                                    "cgValueType": "Number"
+                                }
+                            ]
+                        }
+                    }
+                },
+                {
+                    "item": {
+                        "name": "format_string",
+                        "icon": "fa fa-square",
+                        "data": {
+                            "cgType": "Expression",
+                            "cgName": "format_string",
+                            "cgInputs": [
+                                {
+                                    "cgType": "Point",
+                                    "cgName": "format",
+                                    "cgValueType": "String"
+                                }
+                            ],
+                            "cgOutputs": [
+                                {
+                                    "cgType": "Point",
+                                    "cgName": "value",
+                                    "cgValueType": "String"
+                                }
+                            ]
+                        }
+                    }
+                }
+            ]
+        }
+    },
+    {
+        "group": {
+            "name": "Built-ins",
+            "items": [
+                {
+                    "item": {
+                        "name": "Condition",
+                        "icon": "fa fa-share-alt",
+                        "data": {
+                            "cgType": "Condition",
+                            "cgName": "Condition",
+                            "cgInputs": [
+                                {
+                                    "cgType": "Stream",
+                                    "cgName": "in"
+                                },
+                                {
+                                    "cgType": "Point",
+                                    "cgName": "test",
+                                    "cgValueType": "Boolean"
+                                }
+                            ],
+                            "cgOutputs": [
+                                {
+                                    "cgType": "Stream",
+                                    "cgName": "true"
+                                },
+                                {
+                                    "cgType": "Stream",
+                                    "cgName": "false"
+                                }
+                            ]
+                        }
+                    }
+                },
+                {
+                    "item": {
+                        "name": "Each",
+                        "icon": "fa fa-refresh",
+                        "data": {
+                            "cgType": "Each",
+                            "cgName": "Each",
+                            "cgInputs": [
+                                {
+                                    "cgType": "Stream",
+                                    "cgName": "in"
+                                },
+                                {
+                                    "cgType": "Point",
+                                    "cgName": "list",
+                                    "cgValueType": "List"
+                                }
+                            ],
+                            "cgOutputs": [
+                                {
+                                    "cgType": "Stream",
+                                    "cgName": "iteration"
+                                },
+                                {
+                                    "cgType": "Stream",
+                                    "cgName": "end"
+                                }
+                            ]
+                        }
+                    }
+                },
+                {
+                    "item": {
+                        "name": "Range",
+                        "icon": "fa fa-retweet",
+                        "data": {
+                            "cgType": "Range",
+                            "cgName": "Range",
+                            "cgInputs": [
+                                {
+                                    "cgType": "Stream",
+                                    "cgName": "in"
+                                },
+                                {
+                                    "cgType": "Point",
+                                    "cgName": "from",
+                                    "cgValueType": "Number"
+                                },
+                                {
+                                    "cgType": "Point",
+                                    "cgName": "to",
+                                    "cgValueType": "Number"
+                                },
+                                {
+                                    "cgType": "Point",
+                                    "cgName": "step",
+                                    "cgValueType": "Number"
+                                }
+                            ],
+                            "cgOutputs": [
+                                {
+                                    "cgType": "Stream",
+                                    "cgName": "iteration"
+                                },
+                                {
+                                    "cgType": "Stream",
+                                    "cgName": "end"
+                                }
+                            ]
+                        }
+                    }
+                }
+            ]
+        }
+    }
+];
