36d8d13b7a59ee1b17510b6b06960cff0aa5242d
JonathanPicques <jonathan.picques@gmail.com>
2016-03-10 10:59:47 +0100

    Added variables and conditions models

--- demo/audigame/data.js
+++ demo/audigame/data.js
@@ -1,5 +1,87 @@
 const DUDE_GRAPH_MODELS = [
     {
+        "group": {
+            "name": "Variables",
+            "items": [
+                {
+                    "item": {
+                        "name": "player",
+                        "icon": "fa fa-circle",
+                        "data": {
+                            "blockType": "Variable",
+                            "blockValueType": "Object",
+                            "blockValue": "player",
+                            "blockName": "player",
+                            "blockInputs": [],
+                            "blockOutputs": [
+                                {
+                                    "pointType": "Point",
+                                    "pointValueType": "Object",
+                                    "pointName": "value"
+                                }
+                            ]
+                        }
+                    }
+                }
+            ]
+        }
+    },
+    {
+        "group": {
+            "name": "Conditions",
+            "items": [
+                {
+                    "item": {
+                        "name": "format",
+                        "icon": "fa fa-square",
+                        "data": {
+                            "blockType": "Format",
+                            "blockName": "format",
+                            "blockInputs": [
+                                {
+                                    "pointType": "Point",
+                                    "pointName": "format",
+                                    "pointValueType": "String"
+                                }
+                            ],
+                            "blockOutputs": [
+                                {
+                                    "pointType": "Point",
+                                    "pointName": "value",
+                                    "pointValueType": "String"
+                                }
+                            ]
+                        }
+                    }
+                },
+                {
+                    "item": {
+                        "name": "expression",
+                        "icon": "fa fa-square",
+                        "data": {
+                            "blockType": "Expression",
+                            "blockName": "expression",
+                            "blockInputs": [
+                                {
+                                    "pointType": "Point",
+                                    "pointName": "expression",
+                                    "pointValueType": "String"
+                                }
+                            ],
+                            "blockOutputs": [
+                                {
+                                    "pointType": "Point",
+                                    "pointName": "value",
+                                    "pointValueType": "Number"
+                                }
+                            ]
+                        }
+                    }
+                }
+            ]
+        }
+    },
+    {
         "item": {
             "name": "Start",
             "icon": "fa fa-stop",
@@ -133,19 +215,16 @@ const DUDE_GRAPH_MODELS = [
         }
     }
 ];
-const DUDE_GRAPH_DEFAULT_GRAPH_DATA = {};
-const DUDE_GRAPH_DEFAULT_RENDERER_DATA = {};
-const DUDE_GRAPH_BLOCK_TYPES = [];
-const DUDE_GRAPH_POINT_TYPES = [
-    {"point": "Stream", "type": dudeGraph.Stream}
-];
-const DUDE_GRAPH_RENDER_BLOCK_TYPES = [];
+const DUDE_GRAPH_DEFAULT_GRAPH_DATA = {"blocks":[{"blockType":"Start","blockId":"3ac0-bbf6-4485-0717-1691-7cf1b314ccd9","blockName":"Start","blockOutputs":[{"pointType":"Stream","pointName":"first","pointValueType":"Stream","pointValue":null,"pointTemplate":null,"pointSingleConnection":true},{"pointType":"Stream","pointName":"second","pointValueType":"Stream","pointValue":null,"pointTemplate":null,"pointSingleConnection":true}],"blockInputs":[{"pointType":"Point","pointName":"text","pointValueType":"String","pointValue":null,"pointTemplate":null,"pointSingleConnection":true},{"pointType":"Point","pointName":"timer","pointValueType":"Number","pointValue":5,"pointTemplate":null,"pointSingleConnection":true},{"pointType":"Point","pointName":"sound","pointValueType":"Resource","pointValue":null,"pointTemplate":null,"pointSingleConnection":true},{"pointType":"Point","pointName":"cover","pointValueType":"Resource","pointValue":null,"pointTemplate":null,"pointSingleConnection":true}],"blockTemplates":{}},{"blockType":"Step","blockId":"3ac0-fd39-9c9a-2173-db42-3f68358d07b5","blockName":"Step","blockOutputs":[{"pointType":"Stream","pointName":"first","pointValueType":"Stream","pointValue":null,"pointTemplate":null,"pointSingleConnection":true},{"pointType":"Stream","pointName":"second","pointValueType":"Stream","pointValue":null,"pointTemplate":null,"pointSingleConnection":true}],"blockInputs":[{"pointType":"Stream","pointName":"in","pointValueType":"Stream","pointValue":null,"pointTemplate":null,"pointSingleConnection":true},{"pointType":"Point","pointName":"choice","pointValueType":"String","pointValue":null,"pointTemplate":null,"pointSingleConnection":true},{"pointType":"Point","pointName":"text","pointValueType":"String","pointValue":null,"pointTemplate":null,"pointSingleConnection":true},{"pointType":"Point","pointName":"timer","pointValueType":"Number","pointValue":5,"pointTemplate":null,"pointSingleConnection":true},{"pointType":"Point","pointName":"sound","pointValueType":"Resource","pointValue":null,"pointTemplate":null,"pointSingleConnection":true},{"pointType":"Point","pointName":"cover","pointValueType":"Resource","pointValue":null,"pointTemplate":null,"pointSingleConnection":true}],"blockTemplates":{}},{"blockType":"End","blockId":"3ac0-a711-5c72-3cbb-1b08-922bc8349241","blockName":"End","blockOutputs":[],"blockInputs":[{"pointType":"Stream","pointName":"in","pointValueType":"Stream","pointValue":null,"pointTemplate":null,"pointSingleConnection":true},{"pointType":"Point","pointName":"choice","pointValueType":"String","pointValue":null,"pointTemplate":null,"pointSingleConnection":true},{"pointType":"Point","pointName":"text","pointValueType":"String","pointValue":null,"pointTemplate":null,"pointSingleConnection":true},{"pointType":"Point","pointName":"sound","pointValueType":"Resource","pointValue":null,"pointTemplate":null,"pointSingleConnection":true},{"pointType":"Point","pointName":"cover","pointValueType":"Resource","pointValue":null,"pointTemplate":null,"pointSingleConnection":true}],"blockTemplates":{}},{"blockType":"Format","blockId":"b2cb-9e34-81f6-d812-ba90-daf8548f4fbc","blockName":"format","blockOutputs":[{"pointType":"Point","pointName":"value","pointValueType":"String","pointValue":null,"pointTemplate":null,"pointSingleConnection":true}],"blockInputs":[{"pointType":"Point","pointName":"format","pointValueType":"String","pointValue":"{{name}} est un {{race}}","pointTemplate":null,"pointSingleConnection":true},{"pointType":"Point","pointName":"name","pointValueType":"String","pointValue":"Denis","pointTemplate":null,"pointSingleConnection":true},{"pointType":"Point","pointName":"race","pointValueType":"String","pointValue":null,"pointTemplate":null,"pointSingleConnection":true}],"blockTemplates":{}},{"blockType":"Expression","blockId":"b2cb-e014-bff8-ca37-cde3-005745caedb7","blockName":"expression","blockOutputs":[{"pointType":"Point","pointName":"value","pointValueType":"Number","pointValue":null,"pointTemplate":null,"pointSingleConnection":true}],"blockInputs":[{"pointType":"Point","pointName":"expression","pointValueType":"String","pointValue":"a + b","pointTemplate":null,"pointSingleConnection":true},{"pointType":"Point","pointName":"a","pointValueType":"Number","pointValue":32,"pointTemplate":null,"pointSingleConnection":true},{"pointType":"Point","pointName":"b","pointValueType":"Number","pointValue":64,"pointTemplate":null,"pointSingleConnection":true}],"blockTemplates":{}}],"connections":[{"connectionOutputPoint":"first","connectionOutputBlock":"3ac0-bbf6-4485-0717-1691-7cf1b314ccd9","connectionInputPoint":"in","connectionInputBlock":"3ac0-fd39-9c9a-2173-db42-3f68358d07b5"},{"connectionOutputPoint":"first","connectionOutputBlock":"3ac0-fd39-9c9a-2173-db42-3f68358d07b5","connectionInputPoint":"in","connectionInputBlock":"3ac0-a711-5c72-3cbb-1b08-922bc8349241"},{"connectionOutputPoint":"value","connectionOutputBlock":"b2cb-9e34-81f6-d812-ba90-daf8548f4fbc","connectionInputPoint":"choice","connectionInputBlock":"3ac0-fd39-9c9a-2173-db42-3f68358d07b5"},{"connectionOutputPoint":"value","connectionOutputBlock":"b2cb-e014-bff8-ca37-cde3-005745caedb7","connectionInputPoint":"race","connectionInputBlock":"b2cb-9e34-81f6-d812-ba90-daf8548f4fbc"}]};
+const DUDE_GRAPH_DEFAULT_RENDERER_DATA = {"zoom":{"translate":[1229.3888888888891,369.05555555555554],"scale":1.4444444444444446},"blocks":[{"id":"3ac0-bbf6-4485-0717-1691-7cf1b314ccd9","block":"3ac0-bbf6-4485-0717-1691-7cf1b314ccd9","description":"Start","position":[-510,-185],"parent":null},{"id":"3ac0-fd39-9c9a-2173-db42-3f68358d07b5","block":"3ac0-fd39-9c9a-2173-db42-3f68358d07b5","description":"Step","position":[-221,-28.5],"parent":null},{"id":"3ac0-a711-5c72-3cbb-1b08-922bc8349241","block":"3ac0-a711-5c72-3cbb-1b08-922bc8349241","description":"End","position":[54,-219],"parent":null},{"id":"b2cb-9e34-81f6-d812-ba90-daf8548f4fbc","block":"b2cb-9e34-81f6-d812-ba90-daf8548f4fbc","description":"format","position":[-433,75],"parent":null},{"id":"b2cb-e014-bff8-ca37-cde3-005745caedb7","block":"b2cb-e014-bff8-ca37-cde3-005745caedb7","description":"expression","position":[-462,223],"parent":null}],"groups":[],"connections":[{"connectionIndex":0,"outputName":"first","outputRendererBlockId":"3ac0-bbf6-4485-0717-1691-7cf1b314ccd9","inputName":"in","inputRendererBlockId":"3ac0-fd39-9c9a-2173-db42-3f68358d07b5"},{"connectionIndex":1,"outputName":"first","outputRendererBlockId":"3ac0-fd39-9c9a-2173-db42-3f68358d07b5","inputName":"in","inputRendererBlockId":"3ac0-a711-5c72-3cbb-1b08-922bc8349241"},{"connectionIndex":2,"outputName":"value","outputRendererBlockId":"b2cb-9e34-81f6-d812-ba90-daf8548f4fbc","inputName":"choice","inputRendererBlockId":"3ac0-fd39-9c9a-2173-db42-3f68358d07b5"},{"connectionIndex":3,"outputName":"value","outputRendererBlockId":"b2cb-e014-bff8-ca37-cde3-005745caedb7","inputName":"race","inputRendererBlockId":"b2cb-9e34-81f6-d812-ba90-daf8548f4fbc"}]};
+const DUDE_GRAPH_BLOCK_TYPES = dudeGraph.defaultBlocks;
+const DUDE_GRAPH_POINT_TYPES = dudeGraph.defaultPoints;
+const DUDE_GRAPH_RENDER_BLOCK_TYPES = dudeGraph.defaultRenderBlocks;
 
 (function Blocks() {
     (function Start() {
         /**
-         * This is like function however, it takes a stream in input and output.
-         * In code it would represent function separated by semicolons.
+         * Represents the start of a story
          * @param {dudeGraph.Block.blockDataTypedef} [blockData={}]
          * @class
          * @extends {dudeGraph.Block}
@@ -188,8 +267,7 @@ const DUDE_GRAPH_RENDER_BLOCK_TYPES = [];
     })();
     (function Step() {
         /**
-         * This is like function however, it takes a stream in input and output.
-         * In code it would represent function separated by semicolons.
+         * Represents a step in a story with two choices
          * @param {dudeGraph.Block.blockDataTypedef} [blockData={}]
          * @class
          * @extends {dudeGraph.Block}
@@ -239,8 +317,7 @@ const DUDE_GRAPH_RENDER_BLOCK_TYPES = [];
     })();
     (function End() {
         /**
-         * This is like function however, it takes a stream in input and output.
-         * In code it would represent function separated by semicolons.
+         * Represents a terminal node of a story
          * @param {dudeGraph.Block.blockDataTypedef} [blockData={}]
          * @class
          * @extends {dudeGraph.Block}
@@ -283,7 +360,8 @@ const DUDE_GRAPH_RENDER_BLOCK_TYPES = [];
 (function RenderBlocks() {
     (function AudigameBlock() {
         /**
-         *
+         * Renders blocks with custom colors
+         * @class
          * @extends {dudeGraph.RenderBlock}
          */
         var AudigameBlock = function () {
