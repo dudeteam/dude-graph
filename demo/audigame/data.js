a1c851c3a102bff349282b3b48bad581777907e3
Jeremie Taboada <taboada.jeremie@gmail.com>
2016-03-10 13:00:03 +0100

    Added all function and builtins.

--- demo/audigame/data.js
+++ demo/audigame/data.js
@@ -1,107 +1,5 @@
 const DUDE_GRAPH_MODELS = [
     {
-        "group": {
-            "name": "Variables",
-            "items": [
-                {
-                    "item": {
-                        "name": "has_key",
-                        "icon": "fa fa-circle",
-                        "data": {
-                            "blockType": "Variable",
-                            "blockValueType": "Boolean",
-                            "blockValue": "has_key",
-                            "blockName": "has_key",
-                            "blockInputs": [],
-                            "blockOutputs": [
-                                {
-                                    "pointType": "Point",
-                                    "pointValueType": "Boolean",
-                                    "pointName": "value"
-                                }
-                            ]
-                        }
-                    }
-                },
-                {
-                    "item": {
-                        "name": "nb_coins",
-                        "icon": "fa fa-circle",
-                        "data": {
-                            "blockType": "Variable",
-                            "blockValueType": "Number",
-                            "blockValue": "nb_coins",
-                            "blockName": "nb_coins",
-                            "blockInputs": [],
-                            "blockOutputs": [
-                                {
-                                    "pointType": "Point",
-                                    "pointValueType": "Number",
-                                    "pointName": "value"
-                                }
-                            ]
-                        }
-                    }
-                }
-            ]
-        }
-    },
-    {
-        "group": {
-            "name": "Conditions",
-            "items": [
-                {
-                    "item": {
-                        "name": "format",
-                        "icon": "fa fa-square",
-                        "data": {
-                            "blockType": "Format",
-                            "blockName": "format",
-                            "blockInputs": [
-                                {
-                                    "pointType": "Point",
-                                    "pointName": "format",
-                                    "pointValueType": "String"
-                                }
-                            ],
-                            "blockOutputs": [
-                                {
-                                    "pointType": "Point",
-                                    "pointName": "value",
-                                    "pointValueType": "String"
-                                }
-                            ]
-                        }
-                    }
-                },
-                {
-                    "item": {
-                        "name": "expression",
-                        "icon": "fa fa-square",
-                        "data": {
-                            "blockType": "Expression",
-                            "blockName": "expression",
-                            "blockInputs": [
-                                {
-                                    "pointType": "Point",
-                                    "pointName": "expression",
-                                    "pointValueType": "String"
-                                }
-                            ],
-                            "blockOutputs": [
-                                {
-                                    "pointType": "Point",
-                                    "pointName": "value",
-                                    "pointValueType": "Number"
-                                }
-                            ]
-                        }
-                    }
-                }
-            ]
-        }
-    },
-    {
         "item": {
             "name": "Start",
             "icon": "fa fa-stop",
@@ -232,6 +130,287 @@ const DUDE_GRAPH_MODELS = [
                     }
                 ]
             }
+        }
+    },
+    {
+        "group": {
+            "name": "Variables",
+            "items": [
+                {
+                    "item": {
+                        "name": "has_key",
+                        "icon": "fa fa-circle",
+                        "data": {
+                            "blockType": "Variable",
+                            "blockValueType": "Boolean",
+                            "blockValue": "has_key",
+                            "blockName": "has_key",
+                            "blockInputs": [],
+                            "blockOutputs": [
+                                {
+                                    "pointType": "Point",
+                                    "pointValueType": "Boolean",
+                                    "pointName": "value"
+                                }
+                            ]
+                        }
+                    }
+                },
+                {
+                    "item": {
+                        "name": "nb_coins",
+                        "icon": "fa fa-circle",
+                        "data": {
+                            "blockType": "Variable",
+                            "blockValueType": "Number",
+                            "blockValue": "nb_coins",
+                            "blockName": "nb_coins",
+                            "blockInputs": [],
+                            "blockOutputs": [
+                                {
+                                    "pointType": "Point",
+                                    "pointValueType": "Number",
+                                    "pointName": "value"
+                                }
+                            ]
+                        }
+                    }
+                }
+            ]
+        }
+    },
+    {
+        "group": {
+            "name": "Functions",
+            "items": [
+                {
+                    "item": {
+                        "name": "assign",
+                        "icon": "fa fa-square",
+                        "data": {
+                            "blockType": "Instruction",
+                            "blockName": "assign",
+                            "blockTemplates": {
+                                "AssignTemplate": {
+                                    "valueType": "Number",
+                                    "templates": ["Number", "String", "Boolean"]
+                                }
+                            },
+                            "blockInputs": [
+                                {
+                                    "pointType": "Stream",
+                                    "pointName": "in"
+                                },
+                                {
+                                    "pointType": "Point",
+                                    "pointName": "this",
+                                    "pointTemplate": "AssignTemplate",
+                                    "pointValueType": "Number"
+                                },
+                                {
+                                    "pointType": "Point",
+                                    "pointName": "other",
+                                    "pointTemplate": "AssignTemplate",
+                                    "pointValueType": "Number"
+                                }
+                            ],
+                            "blockOutputs": [
+                                {
+                                    "pointType": "Stream",
+                                    "pointName": "out"
+                                }
+                            ]
+                        }
+                    }
+                },
+                {
+                    "item": {
+                        "name": "random",
+                        "icon": "fa fa-square",
+                        "data": {
+                            "blockType": "Function",
+                            "blockName": "random_range",
+                            "blockInputs": [
+                                {
+                                    "pointType": "Point",
+                                    "pointName": "from",
+                                    "pointValueType": "Number"
+                                },
+                                {
+                                    "pointType": "Point",
+                                    "pointName": "to",
+                                    "pointValueType": "Number"
+                                }
+                            ],
+                            "blockOutputs": [
+                                {
+                                    "pointType": "Point",
+                                    "pointName": "value",
+                                    "pointValueType": "Number"
+                                }
+                            ]
+                        }
+                    }
+                },
+                {
+                    "item": {
+                        "name": "format",
+                        "icon": "fa fa-square",
+                        "data": {
+                            "blockType": "Format",
+                            "blockName": "format",
+                            "blockInputs": [
+                                {
+                                    "pointType": "Point",
+                                    "pointName": "format",
+                                    "pointValueType": "String"
+                                }
+                            ],
+                            "blockOutputs": [
+                                {
+                                    "pointType": "Point",
+                                    "pointName": "value",
+                                    "pointValueType": "String"
+                                }
+                            ]
+                        }
+                    }
+                },
+                {
+                    "item": {
+                        "name": "expression",
+                        "icon": "fa fa-square",
+                        "data": {
+                            "blockType": "Expression",
+                            "blockName": "expression",
+                            "blockInputs": [
+                                {
+                                    "pointType": "Point",
+                                    "pointName": "expression",
+                                    "pointValueType": "String"
+                                }
+                            ],
+                            "blockOutputs": [
+                                {
+                                    "pointType": "Point",
+                                    "pointName": "value",
+                                    "pointValueType": "Number"
+                                }
+                            ]
+                        }
+                    }
+                }
+            ]
+        }
+    },
+    {
+        "group": {
+            "name": "Built-ins",
+            "items": [
+                {
+                    "item": {
+                        "name": "Condition",
+                        "icon": "fa fa-share-alt",
+                        "data": {
+                            "blockType": "Condition",
+                            "blockName": "Condition",
+                            "blockInputs": [
+                                {
+                                    "pointType": "Stream",
+                                    "pointName": "in"
+                                },
+                                {
+                                    "pointType": "Point",
+                                    "pointName": "test",
+                                    "pointValueType": "Boolean"
+                                }
+                            ],
+                            "blockOutputs": [
+                                {
+                                    "pointType": "Stream",
+                                    "pointName": "true"
+                                },
+                                {
+                                    "pointType": "Stream",
+                                    "pointName": "false"
+                                }
+                            ]
+                        }
+                    }
+                },
+                {
+                    "item": {
+                        "name": "Each",
+                        "icon": "fa fa-refresh",
+                        "data": {
+                            "blockType": "Each",
+                            "blockName": "Each",
+                            "blockInputs": [
+                                {
+                                    "pointType": "Stream",
+                                    "pointName": "in"
+                                },
+                                {
+                                    "pointType": "Point",
+                                    "pointName": "list",
+                                    "pointValueType": "List"
+                                }
+                            ],
+                            "blockOutputs": [
+                                {
+                                    "pointType": "Stream",
+                                    "pointName": "iteration"
+                                },
+                                {
+                                    "pointType": "Stream",
+                                    "pointName": "end"
+                                }
+                            ]
+                        }
+                    }
+                },
+                {
+                    "item": {
+                        "name": "Range",
+                        "icon": "fa fa-retweet",
+                        "data": {
+                            "blockType": "Range",
+                            "blockName": "Range",
+                            "blockInputs": [
+                                {
+                                    "pointType": "Stream",
+                                    "pointName": "in"
+                                },
+                                {
+                                    "pointType": "Point",
+                                    "pointName": "from",
+                                    "pointValueType": "Number"
+                                },
+                                {
+                                    "pointType": "Point",
+                                    "pointName": "to",
+                                    "pointValueType": "Number"
+                                },
+                                {
+                                    "pointType": "Point",
+                                    "pointName": "step",
+                                    "pointValueType": "Number"
+                                }
+                            ],
+                            "blockOutputs": [
+                                {
+                                    "pointType": "Stream",
+                                    "pointName": "iteration"
+                                },
+                                {
+                                    "pointType": "Stream",
+                                    "pointName": "end"
+                                }
+                            ]
+                        }
+                    }
+                }
+            ]
         }
     }
 ];
